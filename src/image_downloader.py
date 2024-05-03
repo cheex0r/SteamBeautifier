fa1d8b72298d73b6be4a719a816b8d6677f0992b
cheex0r <cheex0r@gmail.com>
2024-05-02 20:17:46 -0700

    Simplifying saving as PNG

--- src/image_downloader.py
+++ src/image_downloader.py
@@ -6,30 +6,13 @@ from PIL import Image
 from io import BytesIO
 
 
-def convert_to_png(image_data):
-    image = Image.open(image_data)
-    if image.format != 'PNG':
-        png_image = image.convert('RGBA')
-        png_image_data = BytesIO()
-        png_image.save(png_image_data, format="PNG")
-        png_image_data.seek(0)
-        return png_image_data
-    else:
-        return image_data
-
 def save_image_as_png(url, filename):
     try:
-        # Send a GET request to the image URL
         response = requests.get(url)
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
             image_data = BytesIO(response.content)
-            png_image_data = convert_to_png(image_data)
-
-            # Open a file in binary write mode
-            with open(filename, 'wb') as file:
-                # Write the image data to the file
-                file.write(response.content)
+            image = Image.open(image_data)
+            image.save(filename, format='PNG')
             print(f"Image saved successfully as {filename}")
         else:
             print(f"Failed to download image from {url}. Status code: {response.status_code}")
