f2873bffafa2e43f19f2c55e8273bd0ebe2a332c
Cheex0r <cheex0r@gmail.com>
2024-05-10 23:51:19 -0700

    Make prompts conditional.

--- src/steam_beautifier.py
+++ src/steam_beautifier.py
@@ -18,10 +18,15 @@ def prompt_for_preferences():
     preferences = {}
     preferences['remove_whats_new'] = input("Remove What's New shelf? (True/False): ").lower() == 'true'
     preferences['launch'] = input("Launch Steam? (True/False): ").lower() == 'true'
-    preferences['bigpicture'] = input("Start Steam in Big Picture mode? (True/False): ").lower() == 'true'
-    preferences['steam_api_key'] = input("Enter your Steam API Key: ")
-    preferences['steamgriddb_api_key'] = input("Enter your SteamGridDB API Key: ")
-    preferences['steam_id'] = int(input("Enter your SteamID64: "))
+    if preferences['launch']:
+        preferences['bigpicture'] = input("Start Steam in Big Picture mode? (True/False): ").lower() == 'true'
+    else:
+        preferences['bigpicture'] = False
+    preferences['download-images'] = input("Download missing grid art? (True/False): ").lower() == 'true'
+    if preferences['download-images']:
+        preferences['steam_api_key'] = input("Enter your Steam API Key: ")
+        preferences['steamgriddb_api_key'] = input("Enter your SteamGridDB API Key: ")
+        preferences['steam_id'] = int(input("Enter your SteamID64: "))
     save_preferences(preferences)
     return preferences
 
