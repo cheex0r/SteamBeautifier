1fd13d14ddea221985991d6e96599bd1e90ec3e7
cheex0r <cheex0r@gmail.com>
2024-08-23 19:51:13 -0700

    Grab reqs from requirements.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
 setup(
     name='steam_beautifier',
     version='0.1.0',
     packages=find_packages('src'),  # Search for packages under 'src'
     package_dir={'': 'src'},        # Root directory for packages is 'src'
     include_package_data=True,
-    install_requires=[
-        'Pillow==10.3.0',
-        'requests==2.31.0',
-    ],
+    install_requires=parse_requirements('requirements.txt'),
     entry_points={
         'console_scripts': [
             'steam-beautifier=main:main',  # Point to the main function in src/main.py
